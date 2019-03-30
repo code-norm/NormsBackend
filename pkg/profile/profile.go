3c6fcd52223bda37181b48c594159f6045e53296
John Marshall <jjmarsha@usc.edu>
2019-03-30 14:30:27 -0700

    pushing to vm

--- pkg/profile/profile.go
+++ pkg/profile/profile.go
@@ -1,55 +1,128 @@
-// package profile
+package profile
 
-// import (
-// 	"database/sql"
-// 	"fmt"
-// 	"net/http"
+import (
+	"database/sql"
+	"net/http"
+	"NormsBackend/pkg/session"
+)
+//SymptomHandler receives the symptoms chosen to add to table
+func SymptomHandler(db *sql.DB, u *User) http.HandlerFunc {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		symptoms := make([]bool, 11)
+		r.ParseForm()
+		for i:=0; i < 11; i++ {
+			symptoms[i] = r.FormValue(i);
+		}
 
-// 	"github.com/jjmarsha/NormsBackend/pkg/session"
-// )
-
-// //VPHandler sends profile information form db
-// func VPHandler(db *sql.DB, u *session.User) http.HandlerFunc {
-// 	fn := func(w http.ResponseWriter, r *http.Request) {
-// 		var viewPosts string
-
-// 		rows, err := db.Query("SELECT 'one' col1, 'two' col2, 3 col3, NULL col4")
-// 		if err != nil {
-// 			fmt.Println("Failed to run query", err)
-// 			return
-// 		}
-
-// 		cols, err := rows.Columns()
-// 		if err != nil {
-// 			fmt.Println("Failed to get columns", err)
-// 			return
-// 		}
-
-// 		// Result is your slice string.
-// 		rawResult := make([][]byte, len(cols))
-// 		result := make([]string, len(cols))
-
-// 		dest := make([]interface{}, len(cols)) // A temporary interface{} slice
-// 		for i := range rawResult {
-// 			dest[i] = &rawResult[i] // Put pointers to each string in the interface slice
-// 		}
-
-// 		for rows.Next() {
-// 			err = rows.Scan(dest...)
-// 			if err != nil {
-// 				fmt.Println("Failed to scan row", err)
-// 				return
-// 			}
-
-// 			for i, raw := range rawResult {
-// 				if raw == nil {
-// 					result[i] = "\\N"
-// 				} else {
-// 					result[i] = string(raw)
-// 				}
-// 			}
-
-// 			fmt.Printf("%#v\n", result)
-// 		}
-// 	}
-// }
+		if symptoms[0]
+		{
+			_, err = db.Query("UPDATE userinfo SET speech = '1' WHERE username = ?",
+				newUser.Uname)
+			if err != nil {
+				// If there is any issue with inserting into the database, return a 500 error
+				fmt.Println(err)
+				return
+			}
+		}
+		if symptoms[1]
+		{
+			_, err = db.Query("UPDATE [table] SET vision = '1' WHERE username = ?",
+				newUser.Uname)
+			if err != nil {
+				// If there is any issue with inserting into the database, return a 500 error
+				fmt.Println(err)
+				return
+			}
+		}
+		if symptoms[2]
+		{
+			_, err = db.Query("UPDATE [table] SET muscle = '1' WHERE username = ?",
+				newUser.Uname)
+			if err != nil {
+				// If there is any issue with inserting into the database, return a 500 error
+				fmt.Println(err)
+				return
+			}
+		}
+		if symptoms[3]
+		{
+			_, err = db.Query("UPDATE [table] SET bladder = '1' WHERE username = ?",
+				newUser.Uname)
+			if err != nil {
+				// If there is any issue with inserting into the database, return a 500 error
+				fmt.Println(err)
+				return
+			}
+		}
+		if symptoms[4]
+		{
+			_, err = db.Query("UPDATE [table] SET depression = '1' WHERE username = ?",
+				newUser.Uname)
+			if err != nil {
+				// If there is any issue with inserting into the database, return a 500 error
+				fmt.Println(err)
+				return
+			}
+		}
+		if symptoms[5]
+		{
+			_, err = db.Query("UPDATE [table] SET memory = '1' WHERE username = ?",
+				newUser.Uname)
+			if err != nil {
+				// If there is any issue with inserting into the database, return a 500 error
+				fmt.Println(err)
+				return
+			}
+		}
+		if symptoms[6]
+		{
+			_, err = db.Query("UPDATE [table] SET attention = '1' WHERE username = ?",
+				newUser.Uname)
+			if err != nil {
+				// If there is any issue with inserting into the database, return a 500 error
+				fmt.Println(err)
+				return
+			}
+		}
+		if symptoms[7]
+		{
+			_, err = db.Query("UPDATE [table] SET mood = '1' WHERE username = ?",
+				newUser.Uname)
+			if err != nil {
+				// If there is any issue with inserting into the database, return a 500 error
+				fmt.Println(err)
+				return
+			}
+		}
+		if symptoms[8]
+		{
+			_, err = db.Query("UPDATE [table] SET reasoning = '1' WHERE username = ?",
+				newUser.Uname)
+			if err != nil {
+				// If there is any issue with inserting into the database, return a 500 error
+				fmt.Println(err)
+				return
+			}
+		}
+		if symptoms[9]
+		{
+			_, err = db.Query("UPDATE [table] SET dizziness = '1' WHERE username = ?",
+				newUser.Uname)
+			if err != nil {
+				// If there is any issue with inserting into the database, return a 500 error
+				fmt.Println(err)
+				return
+			}
+		}
+		if symptoms[10]
+		{
+			_, err = db.Query("UPDATE [table] SET judgement  = '1' WHERE username = ?",
+				newUser.Uname)
+			if err != nil {
+				// If there is any issue with inserting into the database, return a 500 error
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+}
